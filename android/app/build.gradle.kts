c7348d36de0ee0ab00df2b7033f4984928a70773
purwadijaya151 <purwadijaya151@users.noreply.github.com>
2026-04-27 23:09:41 +0700

    test: update Android E2E dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -97,9 +97,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test:core:1.5.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test:core:1.7.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test:rules:1.7.0")
 }
--- android/app/src/androidTest/java/com/unihaz/tracerstudy/TracerStudyWizardTest.kt
+++ android/app/src/androidTest/java/com/unihaz/tracerstudy/TracerStudyWizardTest.kt
@@ -4,6 +4,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -18,7 +19,7 @@ class TracerStudyWizardTest {
 
     @Test
     fun canOpenTracerStudyWizard() {
-        onView(withText("Tracer Study")).perform(click())
-        onView(withText("Langkah 1 dari 6 - Data Pribadi")).check(matches(isDisplayed()))
+        onView(withId(R.id.nav_tracer)).perform(click())
+        onView(withText("Langkah 1 dari 6 - Status Alumni")).check(matches(isDisplayed()))
     }
 }
